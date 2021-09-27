59edeaa6cdd1cc7ad4274c1885222a8302280842
邓江洲 <dengjiangzhouReal@gmail.com>
2021-09-27 10:08:35 +0800

    // 3， 发送信号

--- proj/aaaa/aaaa/ViewController.swift
+++ proj/aaaa/aaaa/ViewController.swift
@@ -15,30 +15,28 @@ class ViewController: UIViewController {
     
     let bag = DisposeBag()
     
-    lazy var tb: UITableView = {
-        let table = UITableView(frame: UIScreen.main.bounds)
-        table.rowHeight = 50
-        table.dataSource = self
-        table.register(UITableViewCell.self, forCellReuseIdentifier: UITableViewCell.id)
-        return table
-    }()
-    
-    
-    //  SchedulerType, 调度者
-    //  gcd timer
-    lazy var timer: Observable<Int> = Observable<Int>.interval(DispatchTimeInterval.milliseconds(200), scheduler: MainScheduler.instance)
-    
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
-        view.addSubview(tb)
-        
-        timer.subscribe(onNext: {val in
-            print(val)
-        }).disposed(by: bag)
+
         
         
+        // 1, 创建序列
+        let signal = Observable<String>.create { observer in
+            
+            // 3， 发送信号
+            observer.onNext("哈哈")
+            
+            
+            
+            return Disposables.create()
+        }
+        
+        
+        
+        // 2， 订阅信号
     }
 
 }
@@ -47,20 +45,3 @@ class ViewController: UIViewController {
 
 
 
-extension ViewController: UITableViewDataSource{
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 60
-    }
-    
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cel = tableView.dequeueReusableCell(withIdentifier: UITableViewCell.id, for: indexPath)
-        cel.textLabel?.text = "\(indexPath.row) . 宫"
-        
-        cel.textLabel?.textColor = UIColor.blue
-        return cel
-    }
-    
-    
-}
---
+++ proj/aaaa/aaaa/ViewControllerThree.swift
@@ -0,0 +1,66 @@
+//
+//  ViewController.swift
+//  aaaa
+//
+//  Created by Jz D on 2021/9/23.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+
+class ViewControllerThree: UIViewController {
+    
+    
+    let bag = DisposeBag()
+    
+    lazy var tb: UITableView = {
+        let table = UITableView(frame: UIScreen.main.bounds)
+        table.rowHeight = 50
+        table.dataSource = self
+        table.register(UITableViewCell.self, forCellReuseIdentifier: UITableViewCell.id)
+        return table
+    }()
+    
+    
+    //  SchedulerType, 调度者
+    //  gcd timer
+    lazy var timer: Observable<Int> = Observable<Int>.interval(DispatchTimeInterval.milliseconds(200), scheduler: MainScheduler.instance)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        view.addSubview(tb)
+        
+        timer.subscribe(onNext: {val in
+            print(val)
+        }).disposed(by: bag)
+        
+        
+    }
+
+}
+
+
+
+
+
+extension ViewControllerThree: UITableViewDataSource{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 60
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cel = tableView.dequeueReusableCell(withIdentifier: UITableViewCell.id, for: indexPath)
+        cel.textLabel?.text = "\(indexPath.row) . 宫"
+        
+        cel.textLabel?.textColor = UIColor.blue
+        return cel
+    }
+    
+    
+}
